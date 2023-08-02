651dfc3a32ffa58a20a735267351b48ee40a870c
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-02 02:04:59 -0300

    limpieza

--- salir.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//Reanudamos la sesión 
-session_start(); 
-//Literalmente la destruimos 
-session_destroy(); 
-//Redireccionamos a index.php (al inicio de sesión) 
-header('Location: index.php');
-?>
