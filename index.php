e3c125b97d3d511c3d6dc024077a0eaeb3aff314
michaelrodriguezuy <hola@olimarteam.uy>
2024-04-12 11:20:31 -0300

    inclusion en el numero de gurises de la categoria sub 9

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 
-require_once('Connections/yerbalito.php');
+require_once ('Connections/yerbalito.php');
 //include('session.php'); //esto lo uso para dar la bienvenida y para cerrar sesion
 
 //include('seguridad.php');  	  
 //      include('funciones.php');	
 //include('funciones2.php');
 
-$query = "SELECT * FROM jugador WHERE idcategoria<>10 AND idcategoria<>9 AND idcategoria<>11";
+$query = "SELECT * FROM jugador WHERE idcategoria<>10 AND idcategoria<>9 ";
 $result = mysqli_query($db, $query);
 $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 $cantJugadores = count($rows);
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -25,87 +25,98 @@ $cantJugadores = count($rows);
     <title>Yerbalito</title>
 
     <!- ----------------------------- estilos CSS ----------------------------- ->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
-        
-    <!- ----------------------------- JS ----------------------------- ->
-    <script src="js/provisorio.js"></script>
+        <link rel="stylesheet" href="css/style.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+
+        <!- ----------------------------- JS ----------------------------- ->
+            <script src="js/provisorio.js"></script>
 
 
-    <!- ----------------------------- FONT AWESOME ----------------------------- ->
-    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
-    
-
-    <!-- fullCalendar 3.8.2-->
-    <link rel="stylesheet" href="css/fullcalendar.min.css">
-  <!--fullCalendar 3.8.3 -->
-  <script src="js/fullcalendar.min.js"></script>
-  <script src="js/es.js"></script>
-
-  
-
-    <!- ----------------------------- GOOGLE FONTS ----------------------------- ->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@400;700&display=swap" rel="stylesheet">    
-    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&family=Signika+Negative:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
-
-    <!-- favicon -->
-    <link rel="shortcut icon" href="favicon.ico">
+            <!- ----------------------------- FONT AWESOME ----------------------------- ->
+                <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 
 
-    <style>
-        
-    </style>
+                <!-- fullCalendar 3.8.2-->
+                <link rel="stylesheet" href="css/fullcalendar.min.css">
+                <!--fullCalendar 3.8.3 -->
+                <script src="js/fullcalendar.min.js"></script>
+                <script src="js/es.js"></script>
 
-   
+
+
+                <!- ----------------------------- GOOGLE FONTS ----------------------------- ->
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@400;700&display=swap"
+                        rel="stylesheet">
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow&family=Signika+Negative:wght@400;700&display=swap"
+                        rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+
+                    <!-- favicon -->
+                    <link rel="shortcut icon" href="favicon.ico">
+
+
+                    <style>
+
+                    </style>
+
+
 
 </head>
 
 <body>
-<header>
-    <nav class="nav">
-      <div class="logo"></div>
-    </nav>
-    <nav class="nav-menu">
-      <ul class="ul">
-        <li class="li-inicio">
-          <a class="a" href="#">Inicio</a>
-          <ul class="ul-submenu">
-            <li class="li"><a class="a" href="acerca.html">Nuestra institución</a></li>
-            <li class="li"><a class="a" href="calendario.php">Cumpleaños</a></li>
-          </ul>
-        </li>
-        <li class="li"><a class="a" href="planteles.html">Planteles</a></li>
-        <li class="li"><a class="a" href="blog.php">Blog</a></li>
-        <li class="li"><a class="a" href="contacto.html">Contacto</a></li>
-        <li class="li"><a class="a" href="login.php">Login</a></li>
-      </ul>
-    </nav>
-  </header>
+    <header>
+        <nav class="nav">
+            <div class="logo"></div>
+        </nav>
+        <nav class="nav-menu">
+            <ul class="ul">
+                <li class="li-inicio">
+                    <a class="a" href="#">Inicio</a>
+                    <ul class="ul-submenu">
+                        <li class="li"><a class="a" href="acerca.html">Nuestra institución</a></li>
+                        <li class="li"><a class="a" href="calendario.php">Cumpleaños</a></li>
+                    </ul>
+                </li>
+                <li class="li"><a class="a" href="planteles.html">Planteles</a></li>
+                <li class="li"><a class="a" href="blog.php">Blog</a></li>
+                <li class="li"><a class="a" href="contacto.html">Contacto</a></li>
+                <li class="li"><a class="a" href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
 
     <!-- MAIN -->
     <main class="main">
 
-    <!-- muestro la hora
+        <!-- muestro la hora
         <h1 class="main-h1" id="h1">
             
         </h1>
 -->
         <p class="p">
-            El Club Yerbalito de Baby Fútbol es una Institución que cuenta con más de 45 años de existencia situada en la ciudad de Treinta y Tres y que hace ocho años logró tramitar y obtener la personería jurídica; se dedica a la práctica de fútbol infantil, donde recibe semanalmente a más de 150 niños y niñas, pero hace un par de años viene desarrollando otras actividades conexas en beneficio del crecimiento físico y mental de los niños que en ésta participan, lo cual le permite realizar una tarea social considerada muy importante para la sociedad.
-            </p>
+            El Club Yerbalito de Baby Fútbol es una Institución que cuenta con más de 45 años de existencia situada en
+            la
+            ciudad de Treinta y Tres y que hace ocho años logró tramitar y obtener la personería jurídica; se dedica a
+            la
+            práctica de fútbol infantil, donde recibe semanalmente a más de 150 niños y niñas, pero hace un par de años
+            viene
+            desarrollando otras actividades conexas en beneficio del crecimiento físico y mental de los niños que en
+            ésta
+            participan, lo cual le permite realizar una tarea social considerada muy importante para la sociedad.
+        </p>
 
 
-           <seccion class="seccion-img-institucional">
+        <seccion class="seccion-img-institucional">
             <div class="contenedor-img-institucional">
-              <img src="assets/institucion/1.webp" alt="" class="img">
-              <img src="assets/institucion/2.webp" alt="" class="img">
-              <img src="assets/institucion/3.webp" alt="" class="img">
-              <img src="assets/institucion/4.webp" alt="" class="img">
+                <img src="assets/institucion/1.webp" alt="" class="img">
+                <img src="assets/institucion/2.webp" alt="" class="img">
+                <img src="assets/institucion/3.webp" alt="" class="img">
+                <img src="assets/institucion/4.webp" alt="" class="img">
             </div>
-           </seccion>
+        </seccion>
 
     </main>
 
@@ -138,10 +149,10 @@ $cantJugadores = count($rows);
                 </li>
                 <li>
                     <a href="mailto:info@yerbalito.uy?subject=enviado%20desde%20la%20web" target="_blank"> <i
-                            class="fa fa-envelope" ></i></a>
+                            class="fa fa-envelope"></i></a>
                 </li>
             </ul>
-            
+
         </div>
 
         <div class="footer-bottom">
